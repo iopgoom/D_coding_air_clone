acebdab0dc92df57cf0efd1b9006d4643267fcb9
D_coding <iopgoom@naver.com>
2022-02-03 18:39:40 +0900

    14.4 en

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 app_name = "user"
 
-urlpatterns = [path("login", views.LoginView.as_view(), name="login")]
+urlpatterns = [
+    path("login", views.LoginView.as_view(), name="login"),
+    path("logout", views.log_out, name="log_out"),
+]
