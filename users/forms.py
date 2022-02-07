f30a1b31d3641ae5236f794e627e2c27a4731323
D_coding <iopgoom@naver.com>
2022-02-07 21:33:13 +0900

    15.1 en

--- users/forms.py
+++ users/forms.py
@@ -17,3 +17,39 @@ class LoginForm(forms.Form):
                 self.add_error("password", forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User does not exist"))
+
+
+class SignUpForm(forms.Form):
+    last_name = forms.CharField()
+    first_name = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="confirm password")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError("이메일중복")
+        except models.User.DoesNotExist:
+            return email
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+
+        if password != password1:
+            raise forms.ValidationError("password is not equal")
+        else:
+            return password
+
+    def save(self):
+        last_name = self.cleaned_data.get("last_name")
+        first_name = self.cleaned_data.get("first_name")
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+
+        user = models.User.objects.create_user(email, email, password)
+        user.last_name = last_name
+        user.first_name = first_name
+        user.save()
--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ app_name = "user"
 urlpatterns = [
     path("login", views.LoginView.as_view(), name="login"),
     path("logout", views.log_out, name="log_out"),
+    path("signup", views.SignUpView.as_view(), name="signup"),
 ]
